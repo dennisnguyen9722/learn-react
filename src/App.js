fc81403ae424d823a88d91d3e45ee9b8a3961742
dennisnguyen9722 <dnc221297@gmail.com>
2023-09-11 08:51:03 +0700

    Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from "react";
 import TodoFeature from "./features/Todo";
+import { Route } from "react-router-dom";
+import AlbumFeature from "./features/Song";
 
 function App() {
   return (
     <div className="App">
-      <TodoFeature />
+      Home Page
+      <Route path="/todos" component={TodoFeature} />
+      <Route path="/albums" component={AlbumFeature} />
     </div>
   );
 }
